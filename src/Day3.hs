edb7ea4d8381fb8d4048de8bf469addfb14a8202
Marcus Carlsson <carlsson.marcus@gmail.com>
2017-12-08 16:53:22 +0100

    Day3 - part 2 solution.

--- src/Day3.hs
+++ src/Day3.hs
@@ -1,9 +1,12 @@
 module Main where
 
+import Data.List (mapAccumL)
+import qualified Data.Map as Map
+import Data.Maybe (mapMaybe)
 import Data.Monoid ((<>))
 
 data Direction = U | L | D | R
-data Position = P !Int !Int deriving (Eq, Show)
+data Position = P !Int !Int deriving (Ord, Eq, Show)
 
 positionOf :: Int -> Position
 positionOf p = positions !! (p - 1)
@@ -16,11 +19,22 @@ positions = scanl (\p dir -> move dir p) (P 0 0) base
     base = concat [ replicate n d 
                   | (d, n) <- cycle [R, U, L, D] `zip` concatMap (\x -> [x,x]) [1..] ]
 
-nextDirection :: Direction -> Direction
-nextDirection U = L
-nextDirection L = D
-nextDirection D = R
-nextDirection R = U
+-- Starting at the center, take the sum of the connected neighbours for any previous position in the positions-spiral
+-- Should create a list matching [1, 1, 2, 4, 5, 10, ...]
+sumPositions :: [Int]
+sumPositions = snd $ mapAccumL goNext (Map.singleton (P 0 0) 1) positions
+  where
+    goNext :: Map.Map Position Int -> Position -> (Map.Map Position Int, Int)
+    goNext m p = (Map.insert p currentSum m, currentSum)
+      where
+        currentSum = sum $ mapMaybe (`Map.lookup` m) (neighbours p)
+
+-- Generate neighbours
+neighbours :: Position -> [Position]
+neighbours (P cx cy) = [P (cx + x) (cy + y)
+                       | x <- [-1..1]
+                       , y <- [-1..1]
+                       ]
 
 move :: Direction -> Position -> Position
 move U (P x y) = P x (y + 1)
@@ -33,6 +47,7 @@ main = do
     input <- destination
     let (P x y) = positionOf input
     putStrLn $ "Length to position " <> show input <> ": " <> show (abs x + abs y)
+    putStrLn $ "Part 2: " <> show (head $ dropWhile (<= input) sumPositions)
   where
     destination :: IO Int
     destination = readFile "input/Day3.txt" >>= return . read
