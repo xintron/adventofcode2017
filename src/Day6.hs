b84390d9f0f2e61ec857c569c51eb7828e4b80d4
Marcus Carlsson <carlsson.marcus@gmail.com>
2017-12-20 10:06:08 +0100

    Day 6 - solution 2.

--- src/Day6.hs
+++ src/Day6.hs
@@ -1,17 +1,15 @@
 module Main where
 
-import qualified Data.Set as Set
+import qualified Data.Map as M
 import qualified Data.Vector as V
 import Text.Parsec
 import Text.Parsec.String (Parser, parseFromFile)
 
-cycles :: [Int] -> Int
-cycles xs = next 1 (V.fromList xs) (Set.singleton $ V.fromList xs)
+cycles :: [Int] -> [V.Vector Int]
+cycles xs = iterate next (V.fromList xs)
    where
-     next :: Int -> V.Vector Int -> Set.Set (V.Vector Int) -> Int
-     next i ds mem = if Set.member nv mem
-        then i
-        else next (i + 1) nv (Set.insert nv mem)
+     next :: V.Vector Int -> V.Vector Int
+     next ds = nv
       where
         maxInt = V.maxIndex ds
         nv = redistribute maxInt (ds V.! maxInt) (ds V.// [(maxInt, 0)])
@@ -22,6 +20,13 @@ redistribute :: Int -> Int -> V.Vector Int -> V.Vector Int
 redistribute index amount ds = V.accum (+) ds [(x `mod` length ds, 1)
                                               | x <- [index+1 .. index+amount]
                                               ]
+
+run :: [Int] -> (Int, Int)
+run ds = go 0 M.empty $ cycles ds
+  where
+    go n mem (x:xs) = if M.member x mem
+        then (n, n - mem M.! x)
+        else go (n + 1) (M.insert x n mem) xs
 
 parser :: Parser [Int]
 parser = (numbers `sepBy1` oneOf " \t") <* eof
@@ -37,4 +42,6 @@ withData file p = do
 main :: IO ()
 main = do
     withData "input/Day6.txt" parser >>= \input -> do
-        putStrLn $ "Solution 1: " ++ show (cycles input)
+        let res = run input
+        putStrLn $ "Solution 1: " ++ show (fst res)
+        putStrLn $ "Solution 2: " ++ show (snd res)
